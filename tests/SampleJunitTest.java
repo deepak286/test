4b2052473b366e2fedc8da8b120fa5a03c271fb2
deepak286 <43613673+deepak286@users.noreply.github.com>
2019-04-12 15:43:07 +0530

    Create SampleJunitTest.java

---
+++ tests/SampleJunitTest.java
@@ -0,0 +1,13 @@
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runners.Parameterized;
+
+public class SampleJunitTest{
+  @Test
+  public void test1()
+  {
+      Assertions.assertTrue(true);
+  }
+}
